f620b7fcbd3e32d47f6aca99cb348c89467456f7
Jon Hutchison <jonmhutch@mac.com>
2021-03-29 16:55:18 -0500

    update

---
+++ build/precache-manifest.bae865e2cf8ca9a5a6df022459371187.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2838cb5c1a15a0a9900f901a634b7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf3d4138fcab73d26a0c",
+    "url": "/static/css/main.9f534e57.chunk.css"
+  },
+  {
+    "revision": "c8fd89fbd9cafea67d13",
+    "url": "/static/js/2.abec8450.chunk.js"
+  },
+  {
+    "revision": "bf3d4138fcab73d26a0c",
+    "url": "/static/js/main.d5118298.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import Column from './components/Column'
 import styled from 'styled-components'
 import Header from './components/Header'
 import Item from './components/Item'
-import Instagram from './components/Instagram'
-import 'rss-parser/dist/rss-parser.min.js';
-
 
 class App extends Component {
   render() {
@@ -17,15 +14,22 @@ class App extends Component {
             <Item>
               <h2>Experience</h2>
               <Item border>
+                <Date>August 2019 - Present</Date>
+                <h3>Leafly - Senior Software Engineer</h3>
+                <p>As a Senior Developer, I've been responsible for the content creation portions of the website including <code>/news</code> pages, the home page and several aspects of the strain finding experience. I helped reduce complexity and old code in Wordpress while using React to take advantage of the powerful Gutenberg editor within Wordpress. I also implemented a new CMS for content outside of our <code>/news</code> pages using Directus.</p>
+                <p>I helped implement and standardize image usage with a custom image component built around Imgix. This resulted in a vast improvement in performance metrics like Lighthouse and Core Web Vitals.</p>
+                <p>Other duties have revolved around iteration on the home page and building out Strain pages to better connect users to dispensaries and products.</p>
+              </Item>
+              <Item border>
                 <Date>January 2019 - July 2019</Date>
-                <h3>Spreetail</h3>
-                <p>Served as a Senior Front End Developer, where I implemented a CMS using Strapi for our home page content, our blog and marketing content throughout the site.</p>
+                <h3>Spreetail - Senior Front End Engineer</h3>
+                <p>I helped implemented a CMS using Strapi for our home page content, our blog and marketing content throughout the site.</p>
                 <p>Helped convert code base from SCSS to Styled Components. Headed up image optimization site wide with the help of Imgix.</p>
                 <p>Gained a firm grasp on code testing with Jest, Enzyme, Selenium and Nightwatch.</p>
               </Item>
               <Item border>
                 <Date>June 2016 - December 2018</Date>
-                <h3>Tastemade</h3>
+                <h3>Tastemade - Software Developer</h3>
                 <p>Feature development and maintenance for <a href="https://www.tastemade.com/">Tastemade.com</a> with a small team of developers and a designer. Primarily working with a React, Webpack, Node stack.</p>
                 <p>Creation and front end development for <a href="https://shop.tastemade.com/">shop.tastemade.com</a> in Shopify using Liquid and Javascript.</p>
                 <p>Featured development and maintenance for Tastemade's Roku and Apple TV 3 applications using Brightscript, Javascript, XML and AWS Lambdas.</p>
@@ -33,7 +37,7 @@ class App extends Component {
               <Item border>
                 <Date>September 2015 - June 2016</Date>
                 <h3>
-                  GPO, inc.
+                  GPO, inc. - Front End Developer
                 </h3>
                 <p>
                   Worked on a small team of developers, copywriters, designers and SEO specialists to produce ultra-performant SEO location pages for companies like Domino's, Maggianos, Check N' Go and Bridgestone. Each microsite used a Node, Handlebars and PostCSS stack.
@@ -42,7 +46,7 @@ class App extends Component {
               <Item>
                 <Date>Jul 2012 – Aug 2015</Date>
                 <h3>
-                  Mutual Mobile
+                  Mutual Mobile - Front End Developer
                 </h3>
                 <p>
                   Developed feature-rich web applications and responsive websites for some of Americas top companies using the HTML5, CSS3, javascript and the companies proprietary MVC framework Lavaca. For a brief period, I transitioned to the marketing department to serve as the marketing engineer where my daily tasks included administering and maintaining Marketo and Salesforce and creating prototypes or new tools such as an app review aggregator that added value for clients and/or drove new business.
@@ -54,23 +58,15 @@ class App extends Component {
             <Item border>
               <h2>Skills</h2>
               <h3>Web Development</h3>
-              <p>HTML5, CSS3, ES6 Javascript, React 16, Redux, Redux Saga, Node, Styled Components, Jest, Enzyme, Selenium, Nightwatch, webpack, babel, axios, react-intl, express, NPM</p>
-              <h3>Analytics</h3>
-              <p>Google Analytics, AWS, Looker, Amplitude, GTM, Dynamic Yield</p>
+              <p>HTML5, CSS3, ES6 Javascript, React, Redux, Node.js, Next.js, Jest, Enzyme, Selenium, Nightwatch, webpack, babel, axios</p>
+              <h3>Analytics, A/B Testing, Tagging, Logging</h3>
+              <p>Google Analytics, Split.io, Looker, Amplitude, GTM, Datadog, Rollbar</p>
               <h3>Other</h3>
-              <p>Handlebars, PHP, BrightScript, XML, AWS Lambda</p>
+              <p>Wordpress, PHP, AWS Lambda</p>
             </Item>
             <Item border>
               <h2>Interests</h2>
-              <p>Responsive web development, data science and visualization, web accessibility, user experience design, web animations, interactive media and its various web implementations</p>
-            </Item>
-            <Item border>
-              <h2>What I'm Learning Next</h2>
-              <p>Typescript, GraphQL, React Native</p>
-            </Item>
-            <Item>
-              <h2>Outside of Work</h2> 
-              <Instagram />
+              <p>Responsive web development, Front end development, accessibility, performance</p>
             </Item>
           </Column>
         </ColumnContainer>
